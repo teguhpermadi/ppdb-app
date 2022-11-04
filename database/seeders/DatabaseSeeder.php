8cebb853403e6c52ba120814961c0bc7542ffb29
Teguh Permadi <tguh.permadi@gmail.com>
2022-11-04 20:59:10 +0700

    user roles factory seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            RolesSeeder::class,
+            AdminSeeder::class,
+            UsersSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=0; $i < 5; $i++) { 
+            # code...
+            $user = User::factory()->create();
+            $user->assignRole('user');
+        }
+    }
+}
